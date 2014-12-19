mikesmullin/borg

commit 3b4e4d48d7e523dd780bcef09e49b848bcfd75db

fix bug in network.coffee flatten; instance should override last but cant if it is also first

Borg.coffee
@@ -81,7 +81,7 @@ class Borg
 
     # traverse and expand network hierarchy
     @eachServer ({ datacenter, group, type, instance, env, tld, subproject, server }) =>
-      _.merge server,
+      server = _.merge {},
         @networks.global,
         _.omit @networks.datacenters[datacenter], 'groups'
         _.omit @networks.datacenters[datacenter].groups[group], 'servers'

commit 03b23e771b90bd73cc2bf2c4d152ea26bcc98c0a

fix regression bug introduced in 3b4e4d48 need to merge server object completely separately

Borg.coffee
@@ -81,12 +81,13 @@ class Borg
 
     # traverse and expand network hierarchy
     @eachServer ({ datacenter, group, type, instance, env, tld, subproject, server }) =>
-      server = _.merge {},
+      d = _.merge {},
         @networks.global,
         _.omit @networks.datacenters[datacenter], 'groups'
         _.omit @networks.datacenters[datacenter].groups[group], 'servers'
         _.omit @networks.datacenters[datacenter].groups[group].servers[type], 'instances'
         server
+      _.merge server, d
 
       # plus a few implicitly calculated attributes
       server.datacenter ||= datacenter