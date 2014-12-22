mikesmullin/borg

commit 07fff38ab9e9b8d23de59a42a0755e770315baa2

better version of remember write which merges object values instead of replacing them

Borg.coffee
@@ -223,7 +223,7 @@ class Borg
           if value is null # delete
             delete pointer[key]
           else # write
-            pointer[key] = value
+            pointer[key] = _.merge {}, pointer[key], value
           fs.writeFileSync memory_file, JSON.stringify memory, null, 2
           return
       else

commit 32300ee9dc9cee98395ce1d8c9ff8f57f2f91231

fix regression bug introduced by 07fff38a can only merge Objects; all other values must override

Borg.coffee
@@ -234,7 +234,10 @@ class Borg
           if value is null # delete
             delete pointer[key]
           else # write
-            pointer[key] = _.merge {}, pointer[key], value
+            if typeof pointer[key] is 'object' and typeof value is 'object'
+              pointer[key] = _.merge {}, pointer[key], value # merge existing objects
+            else
+              pointer[key] = value # otherwise override
           fs.writeFileSync memory_file, JSON.stringify memory, null, 2
           return
       else